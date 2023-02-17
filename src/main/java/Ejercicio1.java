1b1c5bbb1ce41aac508fe5029e84e85f6433c938
Eloy Sánchez Tamayo <esanctam@myuax.com>
2023-02-17 14:03:53 +0100

    Ejercicio 1.V2

--- src/main/java/Ejercicio1.java
+++ src/main/java/Ejercicio1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Ejercicio1_V1 {
+public class Ejercicio1 {
     /*Este algoritmo calcula la media de valores (por ejemplo, de las notas), introducidos por el usuario. En primer lugar, el usuario indica el número de valores cuyo promedio desea conocer. Seguidamente, introduce todos estos valores. Finalmente, se muestra la media de estos valores.
 
 A continuación se muestra un ejemplo de posibles visualizaciones y valores introducidos, durante una ejecución del algoritmo (los valores introducidos por el usuario, están escritos en negrita y cursiva):
@@ -23,9 +23,8 @@ Nota:
 
 La media es 14,5
 
-     */
+
     public static void main(String[] args) {
-        // TODO code application logic here
         int n;
         double nota, suma, media;
         Scanner teclado = new Scanner(System.in);
@@ -39,5 +38,43 @@ La media es 14,5
         }
         media = suma / n;
         System.out.println("La media es " + media);
+    }
+
+     */
+    /*
+    El objetivo sigue siendo realizar el cálculo de la media de un conjunto de valores introducidos por el usuario, pero el usuario ya no tiene que indicar inicialmente el número de valores. Introduce los valores y cuando ha terminado, introduce un valor concreto (-1), para indicar que ha terminado de introducir valores.
+
+Ejemplo de ejecución del algoritmo:
+
+¿Nota (-1 para terminar)?
+
+9
+
+¿Nota (-1 para terminar)?
+
+18
+
+¿Nota (-1 para terminar)?
+
+-1
+
+La nota media es 13,5
+     */
+    public static void main(String[] args) {
+        double nota, suma, media;
+        int n;
+        Scanner teclado = new Scanner(System.in);
+        suma = 0;
+        n = 0;
+        System.out.println("¿Nota (-1 para terminar)?");
+        nota = teclado.nextDouble();
+        while (nota != -1) {
+            suma = suma + nota;
+            n = n + 1;
+            System.out.println("¿Nota (-1 para terminar)?");
+            nota = teclado.nextDouble();
+        }
+        media = suma / n;
+        System.out.println("La nota media es " + media);
     }
 }
