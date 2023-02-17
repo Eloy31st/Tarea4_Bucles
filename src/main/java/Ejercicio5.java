c4f877b7d0dc9b1ad4862408fd8cedc1b475755a
Eloy Sánchez Tamayo <esanctam@myuax.com>
2023-02-17 15:43:03 +0100

    Ejercicio 5.V2

--- src/main/java/Ejercicio5.java
+++ src/main/java/Ejercicio5.java
@@ -34,6 +34,7 @@ Adios
 
 ¡Por supuesto, puede elegir las películas y las citas que prefiera!
      */
+    /*
     public static void main(String[] args) {
         int opcion;
         Scanner teclado = new Scanner(System.in);
@@ -64,5 +65,76 @@ Adios
                     System.out.println("Opción incorrecta");
             }
         } while (opcion != 5);
+    }
+
+     */
+    /*
+    Retomar el algoritmo anterior, modificándolo para que no se muestre siempre la misma cita de una película, sino que haya dos o tres y presente aleatoriamente una de ellas.
+     */
+    public static void main(String[] args) {
+        int opcion;
+        Scanner teclado = new Scanner(System.in);
+        do {
+            System.out.println("1 - Una cita de la ciudad del miedo");
+            System.out.println("2 - Una cita de James Bond");
+            System.out.println("3 - Una cita de la vida es un largo río tranquilo");
+            System.out.println("4 - Una cita de Star Wars");
+            System.out.println("5 - Salir de esta magnífica aplicación");
+            opcion = teclado.nextInt();
+            switch (opcion) {
+                case 1:
+                    switch ((int) (Math.random() * 3)) {
+                        case 0:
+                            System.out.println("Cuidado, es una auténtica carnicería");
+                            break;
+                        case 1:
+                            System.out.println("Lo peor de los miedos es que te hace prisionero y te impide hacer lo que quieres");
+                            break;
+                        case 2:
+                            System.out.println("La gente teme lo que no comprende");
+                            break;
+                    }
+                    break;
+                case 2:
+                    switch ((int) (Math.random() * 3)) {
+                        case 0:
+                            System.out.println("Me llamo Bond, James Bond");
+                            break;
+                        case 1:
+                            System.out.println("Ponga las dos manos en el volante, soy un pasajero muy nervioso.");
+                            break;
+                        case 2:
+                            System.out.println("¿Por qué las chinas tenéis distinto sabor que las otras chicas? Sois diferentes, como el pato de Pekín es distinto del caviar ruso, pero las dos cosas me encantan.");
+                            break;
+                    }
+                    break;
+                case 3:
+                    switch ((int) (Math.random() * 3)) {
+                        case 0:
+                            System.out.println("Es sorprendete como el dinero puede encontrar un camino");
+                            break;
+                        case 1:
+                            System.out.println("La vida es un largo río tranquilo, agnes");
+                            break;
+                        case 2:
+                            System.out.println("Que es lo que quieres que te diga?");
+                            break;
+                    }
+                    break;
+                case 4:
+                    switch ((int) (Math.random() * 3)) {
+                        case 0:
+                            System.out.println("No, yo soy tu padre");
+                            break;
+                        case 1:
+                            System.out.println("Yo nunca me cuestiono algo… Hasta después de hacerlo");
+                            break;
+                        case 2:
+                            System.out.println("Que la fuerza esté contigo… siempre");
+                            break;
+                    }
+                    break;
+            }
+        }while (opcion != 5);
     }
 }
